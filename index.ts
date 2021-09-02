c7aa5ea3a0ceede271d02a95a3eb206a265ac8e0
nawealvarez <37459045+nawealvarez@users.noreply.github.com>
2021-09-02 16:09:23 -0300

    change port

--- index.ts
+++ index.ts
@@ -7,4 +7,4 @@ app.use(router.routes());
 app.use(router.allowedMethods()); // Siempre que se haga una peticion va a ser permitida por deno
 
 console.log('Server running on port', 3000);
-await app.listen({port: 3000});
+await app.listen({port: 3100});
